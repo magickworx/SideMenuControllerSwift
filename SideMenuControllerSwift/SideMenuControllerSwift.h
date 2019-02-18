7694004009c053520c730e1100e430197ccc446b
MagickWorX <kouichi@magickworx.com>
2019-02-18 17:23:17 +0900

    Initial Commit

---
+++ SideMenuControllerSwift/SideMenuControllerSwift.h
@@ -0,0 +1,19 @@
+//
+//  SideMenuControllerSwift.h
+//  SideMenuControllerSwift
+//
+//  Created by Kouichi ABE (WALL) on 2019/02/18.
+//  Copyright © 2019年 Kouichi ABE (WALL). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SideMenuControllerSwift.
+FOUNDATION_EXPORT double SideMenuControllerSwiftVersionNumber;
+
+//! Project version string for SideMenuControllerSwift.
+FOUNDATION_EXPORT const unsigned char SideMenuControllerSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SideMenuControllerSwift/PublicHeader.h>
+
+
